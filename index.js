48f0519d983adc153b9ea53596ec510a39b6022e
stellar-lambda-0453 <jordanmack.laird@gmail.com>
2018-09-23 21:21:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
-
+function logShout(string){
+  console.log(string.toLowerCase())
+}
 function sayHiToGrandma(string){
   if (string.toLowerCase() === string)
   return "I can't hear you!" //true
