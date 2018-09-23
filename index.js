9a215364e968edbde0c761a2c81a6827eae09aac
stellar-lambda-0453 <jordanmack.laird@gmail.com>
2018-09-23 21:30:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function whisper(string){
 function logShout(string){
   console.log((string).toUpperCase())
 }
+function logWhisper(string){
+  console.log((string).toLowerCase())
+}
 logShout.toUpperCase()
 function sayHiToGrandma(string){
   if (string.toLowerCase() === string)
